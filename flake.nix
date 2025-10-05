fe02e6d27f5a6fedc710ca4177e1c4e20aef2cba
bombrary <bombra108@gmail.com>
2025-10-05 16:32:51 +0900

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "An empty flake template that you can adapt to your own environment";
+
+  # Flake inputs
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0"; # Stable Nixpkgs
+
+  # Flake outputs
+  outputs =
+    { self, ... }@inputs:
+    let
+      # The systems supported for this flake's outputs
+      supportedSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper for providing system-specific attributes
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            # Provides a system-specific, configured Nixpkgs
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              # Enable using unfree packages
+              config.allowUnfree = true;
+            };
+          }
+        );
+    in
+    {
+      # Development environments output by this flake
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          # Run `nix develop` to activate this environment or `direnv allow` if you have direnv installed
+          default = pkgs.mkShell {
+            # The Nix packages provided in the environment
+            packages = with pkgs; [
+              hugo
+            ];
+
+            # Set any environment variables for your development environment
+            env = { };
+
+            # Add any shell logic you want executed when the environment is activated
+            shellHook = "";
+          };
+        }
+      );
+    };
+}
